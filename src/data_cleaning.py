19f7dece506ab0187922c8e9f3a710d3ffbe7bf2
MFaisal077 <mohammadfaisal87897@gmail.com>
2026-02-07 16:42:26 +0000

    push

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -10,7 +10,7 @@ conn = psycopg2.connect(
     password="Faisal@123"
 )
 cursor=conn.cursor()
-'''
+
 ##LCh by line
 df=pd.read_csv(r"data_raw\Lost Customer Hours By Line.csv",skiprows=1, nrows = 10)
 df.rename(columns={df.columns[0]: 'line'}, inplace=True)
@@ -34,7 +34,7 @@ query1=("Insert into lchbycategory(category,lost_customer_hours,year) values(%s,
 for index,row in melted_df1.iterrows():
     value1=(row['category'],row['Lost Customer Hours'],row['year'])
     cursor.execute(query1,value1)
-'''
+
 
 df2=pd.read_csv(r"data_raw\Excess Journey Time.csv",skiprows=1,nrows=10)
 df2.rename(columns={df2.columns[0]:'line'},inplace=True)
@@ -47,8 +47,24 @@ for index,row in melted_df2.iterrows():
     cursor.execute(query2,value2)
 
 
+df3=pd.read_csv(r"data_raw\Escalator Availability.csv",skiprows=1,nrows=10)
+df3.rename(columns={df3.columns[0]:'line'},inplace=True)
+melted_df3=pd.melt(df3,id_vars=["line"],var_name="year",value_name="Escalator Availability(%)")
+melted_df3["Escalator Availability(%)"]=melted_df3["Escalator Availability(%)"].str.replace("%","")
+query3=("Insert into esc_avl(esc_avail,line,year) values(%s,%s,%s)")
+for index,row in melted_df3.iterrows():
+    value3=(row['Escalator Availability(%)'],row['line'],row['year'])
+    cursor.execute(query3,value3)
 
-
+'''
+df4=pd.read_csv(r"data_raw\Customer Satisfation.csv",skiprows=[0,2],nrows=10)
+df4.rename(columns={df4.columns[0]:'line'},inplace=True)
+melted_df4=pd.melt(df4,id_vars=["line"],var_name="year",value_name="Customer Satisfaction")
+query4=("Insert into css(line,year,customer_satisfaction) values (%s,%s,%s)")
+for index,row in melted_df4.iterrows():
+    value4=(row['line'],row['year'],row['Customer Satisfaction'])
+    cursor.execute(query4,value4)
+'''
 
 
 conn.commit()
